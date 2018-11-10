604d2d89fa919e7b8b6c64ada35f7a65092ceb25
sonushahuji4 <sonushahuji4@gmail.com>
2018-11-10 17:34:33 +0530

    Add files via upload

---
+++ Jadoo_vs_Koba.py
@@ -0,0 +1,4 @@
+#problem link
+#https://www.hackerearth.com/practice/python/getting-started/input-and-output/practice-problems/golf/jadoo-vs-koba/
+for i in range(int(ord('F')),int(ord('Q'))):
+    print(i)
