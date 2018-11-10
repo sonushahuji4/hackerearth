70744c0a4b78342d14a8ba781c62fe9a649fddc3
sonushahuji4 <sonushahuji4@gmail.com>
2018-11-10 17:54:50 +0530

    Add files via upload

---
+++ Jadoo_and_DNA_Transcription.py
@@ -0,0 +1,13 @@
+#problem link
+#https://www.hackerearth.com/practice/python/getting-started/input-and-output/practice-problems/golf/jadoo-and-dna-transcription/
+i=input()
+if i == 'C':
+    print('G')
+elif i== 'G':
+    print('C')
+elif i== 'T':
+    print('A')
+elif i== 'A':
+    print('U')
+else:
+    print('Invalid Input')
