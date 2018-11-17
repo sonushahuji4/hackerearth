fbc99017333f00e31d104b72d05bd4435f92a11d
sonushahuji4 <sonushahuji4@gmail.com>
2018-11-17 15:42:23 +0530

    Add files via upload

---
+++ Special_Shop.py
@@ -0,0 +1,13 @@
+#problem link
+#https://www.hackerearth.com/practice/algorithms/searching/linear-search/practice-problems/algorithm/special-shop-69904c91/
+T=int(input())
+l=[]
+while T>0:
+    N, A, B = map(int, input().split())
+    s=N
+    for i in range(N+1):
+        l.append(((A*(i*i))+(B*(s*s))))
+        s-=1
+    print(min(l))
+    l.clear()
+    T-=1
