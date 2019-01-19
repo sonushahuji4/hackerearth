ce03e6def554441530314e991e3064ed4361dfe3
shahuji <sonushahuji4@gmail.com>
2019-01-20 00:41:37 +0530

    new code

--- Frustrated_coders.py
+++ Frustrated_coders.py
@@ -1,25 +1,3 @@
-# N=int(input())
-# coders = list(map(int, input().split()))
-# coders.sort()
-# maxdata=max(coders)
-# indexdata = []
-# j = 0
-# i=0
-# while len(coders) > 0:
-#     if coders[j] != coders[i] and i not in indexdata:
-#         coders.pop(j)
-#         p = i-1
-#         indexdata.append(p)
-#         j = 0
-#         i-=1
-#     else:
-#         i+=1
-#     if coders[i] == maxdata:
-#         coders.pop(j)
-#         break
-# print(sum(coders))
-
-
 N = int(input())
 coders = list(map(int, input().split()))
 coders.sort()
