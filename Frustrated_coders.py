bed379d38d3bf6b456e13f21b14f2a517750eaee
shahuji <sonushahuji4@gmail.com>
2019-01-20 00:42:59 +0530

    new code

--- Frustrated_coders.py
+++ Frustrated_coders.py
@@ -1,13 +1,14 @@
-N = int(input())
-coders = list(map(int, input().split()))
-coders.sort()
-indexdata = []
-l = [int(x) for x in coders]
-
-for i in range(0, N - 1):
-    for j in range(i, N):
-        if coders[i] != coders[j] and (j) not in indexdata:
-            l[i] = 0
-            indexdata.append(j)
-            break
-print(sum(l))
+n=int(input())
+s=list(map(int,input().split()))
+k=[]
+i=0
+j=i
+s=sorted(s,reverse=True)
+while(i!=n and j!=n):
+    if(s[i]>s[j]):
+        k.append(s[j])
+        i+=1
+        j+=1
+    else:
+        j+=1
+print(sum(s)-sum(k))
