b51922037a85cb9882605fa30dd0cd6ee1f3d09d
ShreyasAdhari <46758643+ShreyasAdhari@users.noreply.github.com>
2019-01-21 00:26:23 +0530

    Update Little_Shino_and_Pairs.py

--- Little_Shino_and_Pairs.py
+++ Little_Shino_and_Pairs.py
@@ -1,3 +1,60 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    long long int n,pairs,max,smax;
+    cin>>n;
+    
+    long long int *nums = new long long [n];
+    
+    pairs = n-1;
+    
+    for(long long int i=0;i<n;i++)
+    {
+        cin>>nums[i];
+    }
+    
+    if(nums[0]>nums[1])
+     {
+         max = 0;
+         smax = 1;
+     }
+     
+     else
+     {
+         max = 1;
+         smax = 0;
+     }
+    
+    for(long long int i=2;i<n;i++)
+    {
+        if(nums[i]<nums[smax])
+         continue;
+         
+        else if(nums[i]>nums[max])
+        {
+            smax = max;
+            max = i;
+        }
+        
+        else
+        {
+            smax = i;
+        }
+        
+        if(abs(max-smax)>1)
+         pairs++;
+    }
+    
+    cout<<pairs<<"\n";
+    
+}
+
+
+
 N = int(input())
 data = list(map(int, input().split()))
 data.sort()
